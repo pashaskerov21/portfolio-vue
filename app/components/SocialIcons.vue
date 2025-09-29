f03eeaee3a852675fc64d9056ee1f7959578792d
pasha <askerovpasha21@gmail.com>
2025-09-29 11:20:52 +0400

    social icons

--- app/components/SocialIcons.vue
+++ app/components/SocialIcons.vue
@@ -2,22 +2,22 @@
   <ul v-if="personalData?.social" class="social-icons" :class="[className, { 'menu-active': menuOpen }]">
     <li>
       <a :href="personalData.social.github" target="_blank" rel="noopener noreferrer" aria-label="Github Account">
-        <i class="pi pi-github"></i>
+        <i class="fa-brands fa-github"></i>
       </a>
     </li>
     <li>
       <a :href="personalData.social.linkedin" target="_blank" rel="noopener noreferrer" aria-label="Linkedin Account">
-        <i class="pi pi-linkedin"></i>
+        <i class="fa-brands fa-linkedin-in"></i>
       </a>
     </li>
     <li>
       <a :href="personalData.social.whatsapp" target="_blank" rel="noopener noreferrer" aria-label="Whatsapp Number">
-        <i class="pi pi-whatsapp"></i>
+        <i class="fa-brands fa-whatsapp"></i>
       </a>
     </li>
     <li>
       <a :href="`mailto:${personalData.social.mail}`" target="_blank" rel="noopener noreferrer" aria-label="Mail">
-        <i class="pi pi-at"></i>
+        <i class="fa-solid fa-at"></i>
       </a>
     </li>
   </ul>
